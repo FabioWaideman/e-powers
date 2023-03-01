1495acd9cc071b552541c8d1fd9d09e6a85d5d19
Guilherme Mello <guipcmello@gmail.com>
2023-03-01 17:51:13 -0300

    added deviser

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :orders
   has_many :products
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
