2b7309b64c4156cbfc5d477691ecac58c161687f
Guilherme Mello <guipcmello@gmail.com>
2023-03-02 12:03:39 -0300

    added navbar & fixed devise

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :products
+  resources :products,
+  registrations: "users/registrations"
 end
