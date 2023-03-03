86d8c674616922c7cbb34e910020823b4e791606
Fabio Waideman <fawaideman@yahoo.com.br>
2023-03-03 15:04:24 -0300

    implemented orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :product
   belongs_to :user
+
+  validates :product_id, uniqueness: { scope: :user_id, message: 'Already have this power!!!' }
+  validates :user_id, presence: true
 end
