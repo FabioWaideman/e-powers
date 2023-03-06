b87e4ad686e1706ab5c2c00ba9053fd483b06d99
Fabio Waideman <fawaideman@yahoo.com.br>
2023-03-06 15:44:59 -0300

    added search form

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,16 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_many :orders, dependent: :destroy
+
+  include PgSearch::Model
+  multisearchable against: %i[title description category]
+  pg_search_scope :global_search,
+                  against: {
+                    title: 'A',
+                    description: 'B',
+                    category: 'C'
+                  },
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-# require 'faker'
-
-
-# 10.times do
-#   Product.create(
-#     title: Faker::Superhero.power,
-#     description: Faker::ChuckNorris.fact,
-#     category: Faker::Superhero.suffix,
-#     price: rand(1..300),
-#     user:
-#   )
-# end
